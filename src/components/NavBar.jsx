92dcb1a431c0d0cdbf78e3847dff37f5a45d5bd2
Logesh781 <wlogesh781@gmail.com>
2025-09-14 22:48:59 +0530

    d

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+function NavBar(){
+    return(
+        <nav className="navbar">
+           <div className="navbar-brand">
+            <Link to="/">Film Explore</Link>
+            </div> 
+            <div className="navbar-links">
+                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/favourites" className="nav-link">Favourites</Link>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,7 @@ function Home(){
     const handleSearch = (e) =>{
         e.preventDefault();
         alert(searchQuery)
+        setSearchQuery("")
     }
 
     return(
